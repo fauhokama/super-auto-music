026e62b222623e28d504365cc077edf8d61511ae
Faustino Hokama <fhokama@equoplatform.com>
2022-04-10 20:42:24 -0300

    Add Prisma with basic structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import type { NextPage } from 'next';
+import useSWR from 'swr';
 
 const Home: NextPage = () => {
+	const fetcher = (url: string) => fetch(url).then(res => res.json());
+	const { data, error } = useSWR('/api/battle', fetcher);
+	if (error) return <div>An error occured.</div>;
+	if (!data) return <div>Loading ...</div>;
+
 	return (
 		<div>
 			<h1> Hello! </h1>
