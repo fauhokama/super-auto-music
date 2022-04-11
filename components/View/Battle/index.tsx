67b2d5be154b1894729641f6fb81a60cebfcb5fd
Faustino Hokama <fhokama@equoplatform.com>
2022-04-11 03:39:05 -0300

    Render both teams

--- components/View/Battle/index.tsx
+++ components/View/Battle/index.tsx
@@ -5,9 +5,14 @@ import ElCreature from '../../ElCreature';
 
 const BattleView: FC = () => {
 	const fetcher = (url: string): Promise<Team> => fetch(url).then(res => res.json());
-	const { data, error } = useSWR('/api/getteam', fetcher);
-	if (error) return <div>An error occured.</div>;
-	if (!data) return <div>Loading ...</div>;
+	const { data: playerTeam, error: playerError } = useSWR('/api/getteam', fetcher);
+	const { data: enemyTeam, error: enemyError } = useSWR('/api/getteam?skip=true', fetcher);
+
+	if (playerError) return <div>An error occured.</div>;
+	if (!playerTeam) return <div>Loading ...</div>;
+
+	if (enemyError) return <div>An error occured.</div>;
+	if (!enemyTeam) return <div>Loading ...</div>;
 
 	return (
 		<div>
@@ -16,8 +21,14 @@ const BattleView: FC = () => {
 			</button> */}
 			<div className="flex justify-between">
 				<div>
-					<h1> Team 1 </h1>
-					{data.creatures.map((creature, index) => (
+					<h1> {playerTeam.name} </h1>
+					{playerTeam.creatures.map((creature, index) => (
+						<ElCreature key={index} {...creature} />
+					))}
+				</div>
+				<div>
+					<h1> {enemyTeam.name} </h1>
+					{enemyTeam.creatures.map((creature, index) => (
 						<ElCreature key={index} {...creature} />
 					))}
 				</div>
--- pages/api/getteam.ts
+++ pages/api/getteam.ts
@@ -5,7 +5,14 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	let skip = 0;
+	if (req.query.skip === 'true') {
+		skip = 1;
+	}
+
 	//  This should return a random team
-	const team = await prisma.team.findFirst();
+	const team = await prisma.team.findFirst({
+		skip,
+	});
 	res.json(team);
 }
